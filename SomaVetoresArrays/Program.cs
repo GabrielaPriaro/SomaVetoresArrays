e9e424ae23a04cc17b30d5f32f4d77db5c5a8e7e
Gabriela <gabriela.priaro@gmail.com>
2024-11-04 14:58:59 +0000

    Implementaçao para calcular a soma do vetorA com o VetorB, e mostrar o resultado no VetorC.

--- SomaVetoresArrays/Program.cs
+++ SomaVetoresArrays/Program.cs
@@ -1,10 +1,43 @@
-﻿namespace SomaVetoresArrays
+﻿//Utilizando Arrays:
+//Problema "soma_vetores"
+//Faça um programa para ler dois vetores A e B, contendo N elementos cada. Em seguida, gere um
+//terceiro vetor C onde cada elemento de C é a soma dos elementos correspondentes de A e B. Imprima
+//o vetor C gerado. 
+
+namespace SomaVetoresArrays
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            int n;
+
+            Console.Write("Quantos valores vai ter cada vetor? ");
+            n = int.Parse(Console.ReadLine());
+
+            int[] vetA = new int[n];
+            int[] vetB = new int[n];
+            int[] vetC = new int[n];
+
+            Console.WriteLine("Digite os valores do vetor A:");
+            for (int i = 0; i < n; i++)
+            {
+                vetA[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Digite os valores do vetor B:");
+            for (int i = 0; i < n; i++)
+            {
+                vetB[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("VETOR RESULTANTE:");
+            for (int i = 0; i < n; i++)
+            {
+                vetC[i] = vetA[i] + vetB[i];
+                Console.WriteLine(vetC[i]);
+            }
         }
     }
 }
